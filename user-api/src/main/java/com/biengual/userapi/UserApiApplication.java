978fec67a805b02691f26bae14669c93ebce3779
mooncw <dusrktl4@gmail.com>
2024-10-21 18:37:49 +0900

    Bugfix: 애플리케이션 타임존 설정 추가

--- user-api/src/main/java/com/biengual/userapi/UserApiApplication.java
+++ user-api/src/main/java/com/biengual/userapi/UserApiApplication.java
@@ -3,12 +3,16 @@ package com.biengual.userapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication(scanBasePackages = {
     "com.biengual.userapi"
 })
 public class UserApiApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+
         SpringApplication.run(UserApiApplication.class, args);
     }
 
