1af7c13ced09614ef681cdef887baf9847e61b90
mooncw <dusrktl4@gmail.com>
2024-10-24 21:05:48 +0900

    Feat: 유저 본인 정보 조회 기능 Facade 추가

--- user-api/src/main/java/com/biengual/userapi/user/application/UserFacade.java
+++ user-api/src/main/java/com/biengual/userapi/user/application/UserFacade.java
@@ -2,6 +2,7 @@ package com.biengual.userapi.user.application;
 
 import com.biengual.userapi.annotation.Facade;
 import com.biengual.userapi.user.domain.UserCommand;
+import com.biengual.userapi.user.domain.UserInfo;
 import com.biengual.userapi.user.domain.UserService;
 import lombok.RequiredArgsConstructor;
 
@@ -9,6 +10,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserFacade {
     private final UserService userService;
+
+    // 본인 정보 조회
+    public UserInfo.MyInfo getMyInfo(Long userId) {
+        return userService.getMyInfo(userId);
+    }
 
     // 본인 정보 수정
     public void updateMyInfo(UserCommand.UpdateMyInfo command) {
