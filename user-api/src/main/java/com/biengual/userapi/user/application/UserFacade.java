a62a7bb6a3a24fc1c87cdb2dd65223e04e0e6446
mooncw <dusrktl4@gmail.com>
2024-10-24 23:34:06 +0900

    Feat: 유저 로그인 상태 확인 기능 Facade 구현

--- user-api/src/main/java/com/biengual/userapi/user/application/UserFacade.java
+++ user-api/src/main/java/com/biengual/userapi/user/application/UserFacade.java
@@ -1,6 +1,7 @@
 package com.biengual.userapi.user.application;
 
 import com.biengual.userapi.annotation.Facade;
+import com.biengual.userapi.oauth2.domain.info.OAuth2UserPrincipal;
 import com.biengual.userapi.user.domain.UserCommand;
 import com.biengual.userapi.user.domain.UserInfo;
 import com.biengual.userapi.user.domain.UserService;
@@ -31,5 +32,10 @@ public class UserFacade {
     // 로그아웃
     public void logout(HttpServletRequest request, HttpServletResponse response, Long userId) {
         userService.logout(request, response, userId);
+    }
+
+    // 유저 로그인 상태 확인
+    public boolean getLoginStatus(OAuth2UserPrincipal principal) {
+        return userService.getLoginStatus(principal);
     }
 }
