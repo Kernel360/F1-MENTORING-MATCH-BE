53f0cbea769b7d9bb3b38fa8f1f209df34d451b1
Tigris <kyrae604@naver.com>
2024-10-25 11:01:55 +0900

    [REFACTOR] 리뷰 반영

--- user-api/src/main/java/com/biengual/userapi/swagger/bookmark/SwaggerBookmarkList.java
+++ user-api/src/main/java/com/biengual/userapi/swagger/bookmark/SwaggerBookmarkList.java
@@ -1,8 +1,5 @@
 package com.biengual.userapi.swagger.bookmark;
 
-import java.util.List;
-import java.util.Map;
-
 import com.biengual.userapi.bookmark.presentation.BookmarkResponseDto;
 import com.biengual.userapi.swagger.SwaggerReturnInterface;
 
