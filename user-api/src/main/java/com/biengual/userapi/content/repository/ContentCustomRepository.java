1a49ae204001614536bcc4a850766ee04cad2d45
mooncw <dusrktl4@gmail.com>
2024-10-27 17:59:21 +0900

    Refactor: 컨텐츠 검색 기능 Repository 리팩토링

--- user-api/src/main/java/com/biengual/userapi/content/repository/ContentCustomRepository.java
+++ user-api/src/main/java/com/biengual/userapi/content/repository/ContentCustomRepository.java
@@ -4,11 +4,18 @@ import com.biengual.userapi.content.domain.ContentInfo;
 import com.biengual.userapi.content.domain.ContentStatus;
 import com.biengual.userapi.content.domain.QContentEntity;
 import com.biengual.userapi.scrap.domain.QScrapEntity;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -62,5 +69,77 @@ public class ContentCustomRepository {
                 .orElse(null))
             .filter(Objects::nonNull)
             .toList();
+    }
+
+    // 검색 조건에 맞는 컨텐츠를 조회하기 위한 쿼리
+    public Page<ContentInfo.PreviewContent> findPageBySearch(Pageable pageable, String keyword) {
+        QContentEntity contentEntity = QContentEntity.contentEntity;
+
+        BooleanExpression predicate = getSearchPredicate(keyword, contentEntity);
+
+        return findPreviewPage(pageable, predicate, contentEntity);
+    }
+
+    // Internal Method =================================================================================================
+
+    // TODO: Predicate를 사용하지 않는 경우에는 Override? 아니면 null로 입력?
+    // Preview Page를 위한 공통 Pagination 쿼리
+    private Page<ContentInfo.PreviewContent> findPreviewPage(Pageable pageable, Predicate predicate, QContentEntity contentEntity) {
+        List<ContentInfo.PreviewContent> contents = queryFactory
+            .select(
+                Projections.constructor(
+                    ContentInfo.PreviewContent.class,
+                    contentEntity.id,
+                    contentEntity.title,
+                    contentEntity.url,
+                    contentEntity.contentType,
+                    contentEntity.preScripts,
+                    contentEntity.category.name,
+                    contentEntity.hits
+                )
+            )
+            .from(contentEntity)
+            .where(predicate)
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
+
+        JPAQuery<Long> countQuery = queryFactory.select(contentEntity.id.count())
+            .from(contentEntity)
+            .where(predicate);
+
+        return PageableExecutionUtils.getPage(contents, pageable, countQuery::fetchOne);
+    }
+
+    // Public API에서 사용하는 공통 Predicate
+    private BooleanExpression getPublicContentsPredicate(QContentEntity contentEntity) {
+        return contentEntity.contentStatus.eq(ContentStatus.ACTIVATED);
+    }
+
+    // keyword 검색 비지니스 로직
+    private List<String> splitAndLimitWords(String keyword) {
+        return Arrays.stream(keyword.replace(',', ' ').split("\\s+"))
+            .limit(10)
+            .toList();
+    }
+
+    // 각 word에 대한 Predicate
+    private BooleanExpression getWordPredicate(String word, QContentEntity contentEntity) {
+        return contentEntity.title.containsIgnoreCase(word)
+            .or(contentEntity.preScripts.containsIgnoreCase(word));
+    }
+
+    // 최종 검색 Predicate
+    private BooleanExpression getSearchPredicate(String keyword, QContentEntity contentEntity) {
+        BooleanExpression baseExpression = getPublicContentsPredicate(contentEntity);
+
+        List<String> selectedSearchWords = splitAndLimitWords(keyword);
+
+        BooleanExpression searchExpression = selectedSearchWords.stream()
+            .map(word -> getWordPredicate(word, contentEntity))
+            .reduce(BooleanExpression::or)
+            .orElse(null);
+
+        return baseExpression.and(searchExpression);
     }
 }
