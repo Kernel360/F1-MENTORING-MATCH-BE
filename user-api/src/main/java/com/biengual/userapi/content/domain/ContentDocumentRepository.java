675818d700ea4e94fbbc89afba5ee86c24be1f95
mooncw <dusrktl4@gmail.com>
2024-10-28 02:31:11 +0900

    Comment: ContentDocumentRepository 주석 추가

--- user-api/src/main/java/com/biengual/userapi/content/domain/ContentDocumentRepository.java
+++ user-api/src/main/java/com/biengual/userapi/content/domain/ContentDocumentRepository.java
@@ -6,6 +6,11 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * ContentDocument의 Repository 계층의 인터페이스
+ *
+ * @author 김영래
+ */
 public interface ContentDocumentRepository extends MongoRepository<ContentDocument, ObjectId> {
 
 	Optional<ContentDocument> findById(ObjectId id);
