970d6290dca68d8ed630cbb40d947f61024dfdae
mooncw <dusrktl4@gmail.com>
2024-10-28 14:20:20 +0900

    Remove: 필요 없는 로직 삭제

--- user-api/src/main/java/com/biengual/userapi/content/domain/ContentRepository.java
+++ user-api/src/main/java/com/biengual/userapi/content/domain/ContentRepository.java
@@ -2,5 +2,5 @@ package com.biengual.userapi.content.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContentRepository extends JpaRepository<ContentEntity, Long>, ContentRepositoryCustom {
+public interface ContentRepository extends JpaRepository<ContentEntity, Long> {
 }
