f6af846b12f87e29bcc22bf7ba642265288e9454
Tigris <kyrae604@naver.com>
2024-10-31 09:31:11 +0900

    [REFACTOR] 리뷰 반영 - notnull 메세지 추가

--- user-api/src/main/java/com/biengual/userapi/mission/presentation/UpdateDto.java
+++ user-api/src/main/java/com/biengual/userapi/mission/presentation/UpdateDto.java
@@ -1,15 +1,17 @@
 package com.biengual.userapi.mission.presentation;
 
+import static com.biengual.core.constant.BadRequestMessageConstant.*;
+
 import jakarta.validation.constraints.NotNull;
 
 public class UpdateDto {
 
     public record Request(
-        @NotNull
+        @NotNull(message = NULL_MISSION_LIST_ERROR_MESSAGE)
         boolean oneContent,
-        @NotNull
+        @NotNull(message = NULL_MISSION_LIST_ERROR_MESSAGE)
         boolean bookmark,
-        @NotNull
+        @NotNull(message = NULL_MISSION_LIST_ERROR_MESSAGE)
         boolean quiz
     ) {
     }
