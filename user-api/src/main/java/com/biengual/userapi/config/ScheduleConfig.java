345919b27ebd0b5e26461478f28ccdc260e57b52
Tigris <kyrae604@naver.com>
2024-10-31 12:10:59 +0900

    [REFACTOR] 리뷰 반영

--- user-api/src/main/java/com/biengual/userapi/config/ScheduleConfig.java
+++ user-api/src/main/java/com/biengual/userapi/config/ScheduleConfig.java
@@ -1,10 +1,8 @@
 package com.biengual.userapi.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableAsync
 @Configuration
 @EnableScheduling
 public class ScheduleConfig {
--- user-api/src/main/java/com/biengual/userapi/schedule/application/ScheduleServiceImpl.java
+++ user-api/src/main/java/com/biengual/userapi/schedule/application/ScheduleServiceImpl.java
@@ -1,15 +1,9 @@
 package com.biengual.userapi.schedule.application;
 
-import static com.biengual.core.response.error.code.MissionHistoryErrorCode.*;
-
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.biengual.core.response.error.exception.CommonException;
 import com.biengual.userapi.mission.domain.MissionStore;
 import com.biengual.userapi.missionhistory.domain.MissionHistoryStore;
 import com.biengual.userapi.schedule.domain.ScheduleService;
@@ -29,12 +23,7 @@ public class ScheduleServiceImpl implements ScheduleService {
     @Transactional  // TODO: 위클리 질문
     @Scheduled(cron = "00 00 04 * * *")
     public void scheduleResetMission() {
-        try {
-            CompletableFuture<Void> saveTask = CompletableFuture.runAsync(missionHistoryStore::saveMissionsBeforeReset);
-            saveTask.get();     // TODO: 나중에 작업량 많아 질 경우 spring batch로 전환 검토
-            missionStore.resetMission();
-        } catch (ExecutionException | InterruptedException e) {
-            throw new CommonException(MISSION_HISTORY_SAVE_ERROR);
-        }
+        missionHistoryStore.saveMissionsBeforeReset();
+        missionStore.resetMission();
     }
 }
