143accaf8913a9e5dcde7ed48fa2b67f0f388ad8
Tigris <kyrae604@naver.com>
2024-11-14 16:10:27 +0900

    [REFACTOR] count, updatedAt 을 정답 맞출때 까지만 업데이트하도록 수정

--- user-api/src/main/java/com/biengual/userapi/questionhistory/domain/QuestionHistoryCustomRepository.java
+++ user-api/src/main/java/com/biengual/userapi/questionhistory/domain/QuestionHistoryCustomRepository.java
@@ -2,11 +2,11 @@ package com.biengual.userapi.questionhistory.domain;
 
 import static com.biengual.core.domain.entity.questionhistory.QQuestionHistoryEntity.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
-import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
@@ -20,15 +20,12 @@ public class QuestionHistoryCustomRepository {
         queryFactory
             .update(questionHistoryEntity)
             .set(questionHistoryEntity.count, questionHistoryEntity.count.add(1))
-            .set(questionHistoryEntity.finalTry,
-                new CaseBuilder()
-                    .when(questionHistoryEntity.finalTry.eq(false))
-                    .then(isCorrect)
-                    .otherwise(questionHistoryEntity.finalTry)
-            )
+            .set(questionHistoryEntity.finalTry, isCorrect)
+            .set(questionHistoryEntity.updatedAt, LocalDateTime.now())
             .where(
                 questionHistoryEntity.userId.eq(userId)
                     .and(questionHistoryEntity.questionId.eq(questionId))
+                    .and(questionHistoryEntity.finalTry.eq(false))
             )
             .execute();
     }
