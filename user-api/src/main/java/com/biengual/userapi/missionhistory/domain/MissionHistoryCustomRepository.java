5d4fc1e8d2588e277ced5ac2c7c90b9e28c5113b
mooncw <dusrktl4@gmail.com>
2024-11-14 14:38:49 +0900

    Feat: 월간 미션 달력 조회 기능 Repository 구현

--- user-api/src/main/java/com/biengual/userapi/missionhistory/domain/MissionHistoryCustomRepository.java
+++ user-api/src/main/java/com/biengual/userapi/missionhistory/domain/MissionHistoryCustomRepository.java
@@ -2,8 +2,13 @@ package com.biengual.userapi.missionhistory.domain;
 
 import static com.biengual.core.domain.entity.missionhistory.QMissionHistoryEntity.*;
 
+import java.time.LocalDateTime;
+import java.time.YearMonth;
 import java.util.List;
 
+import com.biengual.core.util.PeriodUtil;
+import com.biengual.userapi.dashboard.domain.DashboardInfo;
+import com.querydsl.core.types.ConstructorExpression;
 import org.springframework.stereotype.Repository;
 
 import com.querydsl.core.types.Projections;
@@ -28,6 +33,37 @@ public class MissionHistoryCustomRepository {
             .from(missionHistoryEntity)
             .where(missionHistoryEntity.userId.eq(userId))
             .orderBy(missionHistoryEntity.createdAt.desc())
+            .fetch();
+    }
+
+    // 기간별 미션 내역을 조회하기 위한 쿼리
+    public List<DashboardInfo.MissionHistory> findMissionHistoryByUserIdInMonth(Long userId, YearMonth yearMonth) {
+        LocalDateTime startOfMonth = PeriodUtil.getStartOfMonth(yearMonth);
+        LocalDateTime endOfMonth = PeriodUtil.getEndOfMonth(yearMonth);
+
+        ConstructorExpression<DashboardInfo.MissionStatus> missionStatusProjection = Projections
+            .constructor(
+                DashboardInfo.MissionStatus.class,
+                missionHistoryEntity.oneCount,
+                missionHistoryEntity.bookmark,
+                missionHistoryEntity.quiz,
+                missionHistoryEntity.count
+            );
+
+        ConstructorExpression<DashboardInfo.MissionHistory> missionHistoryProjection = Projections
+            .constructor(
+                DashboardInfo.MissionHistory.class,
+                missionHistoryEntity.createdAt,
+                missionStatusProjection
+            );
+
+        return queryFactory
+            .select(missionHistoryProjection)
+            .from(missionHistoryEntity)
+            .where(
+                missionHistoryEntity.userId.eq(userId)
+                    .and(missionHistoryEntity.createdAt.between(startOfMonth, endOfMonth))
+            )
             .fetch();
     }
 }
