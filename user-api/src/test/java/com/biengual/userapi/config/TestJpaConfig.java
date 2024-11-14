62583dbf3aa7d9713b140a267f516200683ee5ca
mooncw <dusrktl4@gmail.com>
2024-11-14 10:16:01 +0900

    Revert "[TEST] 조회수 동시성 테스트"

--- user-api/src/test/java/com/biengual/userapi/config/TestJpaConfig.java
+++
@@ -1,9 +0,0 @@
-package com.biengual.userapi.config;
-
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration
-@EntityScan(basePackages = {"com.biengual.core"})
-public class TestJpaConfig {
-}
--- user-api/src/test/java/com/biengual/userapi/repository/OptimisticLockContentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.biengual.userapi.repository;
-
-import com.biengual.core.domain.entity.content.ContentEntityWithVersion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OptimisticLockContentRepository extends JpaRepository<ContentEntityWithVersion, Long> {
-}
