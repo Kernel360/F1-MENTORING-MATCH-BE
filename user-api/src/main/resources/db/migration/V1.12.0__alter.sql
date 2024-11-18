bd192b99b31541643933b39fe09d79020a1b7eba
Tigris <kyrae604@naver.com>
2024-11-18 19:15:07 +0900

    [REFACTOR] 리뷰 반영

--- user-api/src/main/resources/db/migration/V1.12.0__alter.sql
+++ user-api/src/main/resources/db/migration/V1.12.0__alter.sql
@@ -1,13 +1,3 @@
 -- Step 1: Add contentId column to category_learning_history table
 ALTER TABLE category_learning_history
     ADD COLUMN content_id BIGINT;
-
--- Step 2: Update the new content_id column with data from learning_history table
-UPDATE category_learning_history clh
-    JOIN learning_history lh ON clh.user_id = lh.user_id AND clh.learning_time = lh.learning_time
-SET clh.content_id = lh.content_id
-WHERE clh.content_id IS NULL;
-
--- Step 3: Make the content_id column NOT NULL after populating it
-ALTER TABLE category_learning_history
-    MODIFY COLUMN content_id BIGINT NOT NULL;
