d8142f2b44d377f2a2e1d8aaa9cc10e14d9adac9
mooncw <dusrktl4@gmail.com>
2024-11-18 14:06:15 +0900

    Feat: ContentLevelFeedbackHistory Entity 및 Flyway 버전 추가

---
+++ user-api/src/main/resources/db/migration/V1.13.0__create.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `question_history`
+(
+    `id`            BIGINT                         NOT NULL AUTO_INCREMENT,
+    `user_id`       BIGINT                         NOT NULL,
+    `content_id`    BIGINT                         NOT NULL,
+    `content_level` ENUM ('LOW', 'MEDIUM', 'HIGH') NOT NULL,
+    `created_at`    DATETIME(6) DEFAULT NULL,
+    `updated_at`    DATETIME(6) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
