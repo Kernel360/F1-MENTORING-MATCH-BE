a8a103e12fb57948095a0f183bac2936630d3477
Tigris <kyrae604@naver.com>
2024-11-18 18:16:12 +0900

    [REFACTOR] 리뷰 반영

--- biengual-core/src/main/java/com/biengual/core/response/status/RecommenderServiceStatus.java
+++ biengual-core/src/main/java/com/biengual/core/response/status/RecommenderServiceStatus.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 public enum RecommenderServiceStatus implements ServiceStatus {
     // success
     RECOMMENDER_UPDATE_SUCCESS("U-R-001"),
-    RECOMMENDER_CATEGORY_VIEW_SUCCESS("U-R-002")
-
-    // failure,
-    ;
+    RECOMMENDER_CATEGORY_VIEW_SUCCESS("U-R-002");
 
     private final String code;
 
