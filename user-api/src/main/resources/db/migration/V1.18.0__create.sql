871fa0e499c032b8c159a8b0f32f7cc1e52f46d3
mooncw <dusrktl4@gmail.com>
2024-12-03 20:22:23 +0900

    Fix: CategoryLearningProgress Flyway 버전 컬럼 mismatching 해결

--- user-api/src/main/resources/db/migration/V1.18.0__create.sql
+++ user-api/src/main/resources/db/migration/V1.18.0__create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS `user_category_progress`
 (
-    `user_id`               BIGINT NOT NULL,
-    `category_id`           BIGINT NOT NULL,
-    `total_learn_count`     BIGINT NOT NULL DEFAULT 0,
-    `completed_learn_count` BIGINT NOT NULL DEFAULT 0,
+    `user_id`                  BIGINT NOT NULL,
+    `category_id`              BIGINT NOT NULL,
+    `total_learning_count`     BIGINT NOT NULL DEFAULT 0,
+    `completed_learning_count` BIGINT NOT NULL DEFAULT 0,
     PRIMARY KEY (`user_id`, `category_id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
