52f9dc79a5dc157909d430ceb328abe36caf690c
Tigris <kyrae604@naver.com>
2024-12-04 16:13:47 +0900

    [REFACTOR] S3ServiceStatus 수정

--- biengual-core/src/main/java/com/biengual/core/response/status/S3ServiceStatus.java
+++ biengual-core/src/main/java/com/biengual/core/response/status/S3ServiceStatus.java
@@ -5,12 +5,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum S3ServiceStatus implements ServiceStatus {
     // success
-    S3_STORE_SUCCESS("U-R-001"),
-    S3_READ_SUCCESS("U-R-002"),
+    S3_STORE_SUCCESS("U-S3-001"),
+    S3_READ_SUCCESS("U-S3-002"),
 
     // failure
-    S3_STORE_FAILURE("U-R-901"),
-    S3_READ_FAILURE("U-R-902");
+    S3_STORE_FAILURE("U-S3-901"),
+    S3_READ_FAILURE("U-S3-902");
 
     private final String code;
 
