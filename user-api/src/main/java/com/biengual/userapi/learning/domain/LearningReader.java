96c35d6b5517a1bcfdc55c81dd3d4aedbd97107c
mooncw <dusrktl4@gmail.com>
2024-12-04 15:34:26 +0900

    Comment: 주석 추가

--- user-api/src/main/java/com/biengual/userapi/learning/domain/LearningReader.java
+++ user-api/src/main/java/com/biengual/userapi/learning/domain/LearningReader.java
@@ -2,6 +2,11 @@ package com.biengual.userapi.learning.domain;
 
 import com.biengual.userapi.recommender.domain.RecommenderInfo;
 
+/**
+ * Learning 도메인의 DataProvider 계층의 인터페이스
+ *
+ * @author 김영래
+ */
 public interface LearningReader {
     RecommenderInfo.PreviewRecommender findSimilarCategoriesBasedOnLearningHistory(Long userId);
 }
