dd5fc299a37e456e919290ec86f0e29672a907b3
Tigris <kyrae604@naver.com>
2024-12-05 10:42:05 +0900

    [REFACTOR] 사용하지 않는 PointDataMart 삭제 및 Spring Batch 비활성화

--- user-api/src/main/java/com/biengual/userapi/batch/BatchScheduler.java
+++ user-api/src/main/java/com/biengual/userapi/batch/BatchScheduler.java
@@ -1,30 +1,16 @@
-package com.biengual.userapi.batch;
-
-import java.time.LocalDateTime;
-
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import lombok.RequiredArgsConstructor;
-
-@Component
-@EnableScheduling
-@RequiredArgsConstructor
-public class BatchScheduler {
-    private final JobLauncher jobLauncher;
-    private final Job pointDataMartJob;
-
-    @Scheduled(cron = "00 00 01 * * ?") // 매일 새벽 1시에 실행
-    public void runPointDataMartJob() throws Exception {
-        JobParameters jobParameters = new JobParametersBuilder()
-            .addString("createdAt", LocalDateTime.now().minusDays(1).toString())
-            .addLong("time", System.currentTimeMillis())
-            .toJobParameters();
-        jobLauncher.run(pointDataMartJob, jobParameters);
-    }
-}
+// package com.biengual.userapi.batch;
+//
+// import org.springframework.batch.core.launch.JobLauncher;
+// import org.springframework.scheduling.annotation.EnableScheduling;
+// import org.springframework.stereotype.Component;
+//
+// import lombok.RequiredArgsConstructor;
+//
+// // TODO: 같은 이슈에서 사용할 수도 있기 때문에 확인 후 다음 PR 에서도 사용하지 않으면 삭제
+// @Component
+// @EnableScheduling
+// @RequiredArgsConstructor
+// public class BatchScheduler {
+//     private final JobLauncher jobLauncher;
+//
+// }
